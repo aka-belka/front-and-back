10f092f0c8219ab97f1b02e1144996273e4de806
aka-belka <sergeevnau18@outlook.com>
2026-03-26 21:21:51 +0800

    практики 13-14

---
+++ практики13-14/app.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('note-form');
+const input = document.getElementById('note-input');
+const list = document.getElementById('notes-list');
+
+function loadNotes() {
+    const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+    list.innerHTML = notes.map(note => `
+        <div class="note-card">
+            <img src="./icons/pins.png" alt="иконка" class="note-icon-img">
+            <div class="note-text">${note}</div>
+        </div>
+    `).join('');
+}
+
+function addNote(text) {
+    const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+    notes.push(text);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    loadNotes();
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value.trim();
+    if (text) {
+        addNote(text);
+        input.value = '';
+    }
+});
+
+loadNotes();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+        try {
+            const registration = await
+            navigator.serviceWorker.register('sw.js');
+            console.log('ServiceWorker зарегистрирован:', registration.scope);
+        } catch (err) {
+            console.error('Ошибка регистрации ServiceWorker:', err);
+        }
+    });
+}
